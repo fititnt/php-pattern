67b88a55c3bb7b752ba00feaf0fe0a16616e821f
fititnt <emerson@webdesign.eng.br>
2012-02-16 16:55:50 -0200

    Updated FluentInterface/library/ClassFluentInterface ^ methods are in alphabetic order ^ changed _doPrivateMethod() to doPrivateMethod() ^updated referente to source at github.com/fititnt/php-design-pattern

--- FluentInterface/library/ClassFluentInterface.php
+++ FluentInterface/library/ClassFluentInterface.php
@@ -7,145 +7,156 @@
  * @license         <licensetype>. See license.txt
  * @version         <version>
  * 
- * @note            Initial template based on https://github.com/fititnt/template via fititnt
+ * @see https://github.com/fititnt/php-design-pattern
  */
 
 class ClassFluentInterface {
 
-    /**
-     * @var Mixed Generic mixed variable description
-     */
-    private $variable;
+	/**
+	 * Generic mixed variable description
+	 * 
+	 * @var Mixed
+	 */
+	private $variable;
 
-    /**
-     * @var Mixed Generic mixed variable description
-     */
-    public $public;
+	/**
+	 * Generic mixed variable description
+	 * 
+	 * @var Mixed
+	 */
+	public $public;
 
-    /**
-     * @var Integer Generic integer variable description
-     */
-    private $integer;
+	/**
+	 * @var Integer Generic integer variable description
+	 */
+	private $integer;
 
-    /**
-     * @var String Generic string variable description
-     */
-    private $string;
+	/**
+	 * Generic string variable description
+	 * 
+	 * @var String
+	 */
+	private $string;
 
-    /**
-     * @var Array Generic array variable description
-     */
-    private $array;
+	/**
+	 * Generic array variable description
+	 * 
+	 * @var Array
+	 */
+	private $array;
 
-    /**
-     * Initialize values
-     */
-    function __construct() {
-        $this->public = '__construct() started value of $public var';
-    }
+	/**
+	 * Initialize values
+	 */
+	public function __construct() {
+		$this->public = '__construct() started value of $public var';
+	}
 
-    function __destruct() {
-        print "\nClassFluentInterface called __destruct()\n";
-    }
+	/**
+	 * Close execution 
+	 */
+	public function __destruct() {
+		print "\nClassFluentInterface called __destruct()\n";
+	}
 
-    /**
-     * Function to debug $this object
-     *
-     * @param String $method: print_r or, var_dump
-     * @param boolean $format: true for print <pre> tags. Default false
-     * @return void
-     */
-    public function debug($method = 'print_r', $format = FALSE) {
-        if ($format) {
-            echo '<pre>';
-        }
-        if ($method === 'print_r') {
-            print_r($this);
-        } else {
-            var_dump($this);
-        }
-        if ($format) {
-            echo '</pre>';
-        }
-    }
+	/**
+	 * Function to debug $this object
+	 *
+	 * @param String $method: print_r or, var_dump
+	 * @param boolean $format: true for print <pre> tags. Default false
+	 * @return void
+	 */
+	public function debug($method = 'print_r', $format = FALSE) {
+		if ($format) {
+			echo '<pre>';
+		}
+		if ($method === 'print_r') {
+			print_r($this);
+		} else {
+			var_dump($this);
+		}
+		if ($format) {
+			echo '</pre>';
+		}
+	}
 
-    /**
-     * Delete (set to NULL) generic variable
-     * 
-     * @param String $name: name of var do delete
-     * @return Object $this
-     */
-    public function del($name) {
-        $this->$name = NULL;
-        return $this;
-    }
+	/**
+	 * Delete (set to NULL) generic variable
+	 * 
+	 * @param String $name: name of var do delete
+	 * @return Object $this
+	 */
+	public function del($name) {
+		$this->$name = NULL;
+		return $this;
+	}
 
-    /**
-     * Return generic variable
-     * 
-     * @param String $name: name of var to return
-     * @return Mixed this->$name: value of var
-     */
-    public function get($name) {
-        return $this->$name;
-    }
+	/**
+	 * Example of private method. Its a good pratice start with _ (undescore)
+	 *
+	 * @param <vartype> <vardescription>
+	 * @return <returntype> <returndescription>
+	 */
+	private function doPrivateMethod($variable) {
+		$result = '_doPrivateMethod for a ';
+		$result .= $variable;
+		return $result;
+	}
 
-    /**
-     * Set one generic variable the desired value
-     * 
-     * @param String $name: name of var to set value
-     * @param Mixed $value: value to set to desired variable
-     * @return Object $this
-     */
-    public function set($name, $value) {
-        $this->$name = $value;
-        return $this;
-    }
+	/**
+	 * Example of private method.
+	 * Will call one private method
+	 *
+	 * @param <vartype> <vardescription>
+	 * @return <returntype> <returndescription>
+	 */
+	public function doPublicMethod($variable) {
+		$result = 'doPublicMethod is asking ...';
+		$result .= $this->doPrivateMethod($variable);
+		return $result;
+	}
 
-    /**
-     * Set to <variable> the desired value
-     * 
-     * @param Mixed $value: name of var to set
-     * @return Object $this
-     */
-    public function setVariable($value) {
-        $this->variable = $value;
-        return $this;
-    }
+	/**
+	 * Return generic variable
+	 * 
+	 * @param String $name: name of var to return
+	 * @return Mixed this->$name: value of var
+	 */
+	public function get($name) {
+		return $this->$name;
+	}
 
-    /**
-     * Get $variable desired value
-     * 
-     * @param Mixed $name: name of var to return
-     * @return Oject $this->$variable
-     */
-    public function getVariable() {
-        return $this->variable;
-    }
+	/**
+	 * Set one generic variable the desired value
+	 * 
+	 * @param String $name: name of var to set value
+	 * @param Mixed $value: value to set to desired variable
+	 * @return Object $this
+	 */
+	public function set($name, $value) {
+		$this->$name = $value;
+		return $this;
+	}
 
-    /**
-     * Example of private method.
-     * Will call one private method
-     *
-     * @param <vartype> <vardescription>
-     * @return <returntype> <returndescription>
-     */
-    public function doPublicMethod($variable) {
-        $result = 'doPublicMethod is asking ...';
-        $result .= $this->_doPrivateMethod($variable);
-        return $result;
-    }
+	/**
+	 * Set to <variable> the desired value
+	 * 
+	 * @param Mixed $value: name of var to set
+	 * @return Object $this
+	 */
+	public function setVariable($value) {
+		$this->variable = $value;
+		return $this;
+	}
 
-    /**
-     * Example of private method. Its a good pratice start with _ (undescore)
-     *
-     * @param <vartype> <vardescription>
-     * @return <returntype> <returndescription>
-     */
-    private function _doPrivateMethod($variable) {
-        $result = '_doPrivateMethod for a ';
-        $result .= $variable;
-        return $result;
-    }
+	/**
+	 * Get $variable desired value
+	 * 
+	 * @param Mixed $name: name of var to return
+	 * @return Oject $this->$variable
+	 */
+	public function getVariable() {
+		return $this->variable;
+	}
 
 }
